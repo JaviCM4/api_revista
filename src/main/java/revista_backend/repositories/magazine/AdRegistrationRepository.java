e970ff72434f2bd9488bd6018d8ca27cfb3fdf67
JaviCM4 <javic4m@gmail.com>
2026-03-09 13:55:06 -0600

    Remove application-local.properties from repo

--- src/main/java/revista_backend/repositories/magazine/AdRegistrationRepository.java
+++
@@ -1,7 +0,0 @@
-package revista_backend.repositories.magazine;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import revista_backend.models.magazine.AdRegistration;
-
-public interface AdRegistrationRepository extends JpaRepository<AdRegistration, Integer> {
-}
--- src/main/java/revista_backend/services/magazine/interfaces/AdRegistrationService.java
+++
@@ -1,9 +0,0 @@
-package revista_backend.services.magazine.interfaces;
-
-import revista_backend.dto.magazine.request.AdRegistrationCreateRequest;
-import revista_backend.exceptions.ResourceNotFoundException;
-
-public interface AdRegistrationService {
-
-    void create(AdRegistrationCreateRequest dto) throws ResourceNotFoundException;
-}
